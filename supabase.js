17fb39c8faeef9e41c1303deef4777373b14b220
stpeters-ss-rwera <st.petersssrwera2019@gmail.com>
2026-06-17 00:41:16 +0300

    supabase.js

---
+++ supabase.js
@@ -0,0 +1,11 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabaseUrl = 'YOUR_SUPABASE_URL'
+const supabaseKey = 'YOUR_SUPABASE_ANON_KEY'
+
+const supabase = createClient(
+    supabaseUrl,
+    supabaseKey
+)
+
+export default supabase
