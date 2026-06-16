b966311121e969f68bcdd3305d041a1ab8343637
stpeters-ss-rwera <st.petersssrwera2019@gmail.com>
2026-06-17 00:46:53 +0300

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL'
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY'
+const supabaseUrl = 'https://xmwhenrhsfkzhtzugami.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhtd2hlbnJoc2Zremh0enVnYW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MzI2NzgsImV4cCI6MjA5NzIwODY3OH0.9GWqUkVkUIK4sPmdoBYRJdGzHm_rAfa1bHUIcCB0ExA'
 
 const supabase = createClient(
     supabaseUrl,
